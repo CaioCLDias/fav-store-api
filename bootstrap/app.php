8676d64e327b91bec87e84b390ac09737531fc7a
CaioCLDias <caio.lorenzon.dias@gmail.com>
2025-08-27 15:38:13 -0300

    cors config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+        $middleware->use([
+            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
+            \Illuminate\Http\Middleware\TrustHosts::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+        ]);
     })
     ->withProviders([
         App\Providers\AuthServiceProvider::class,
